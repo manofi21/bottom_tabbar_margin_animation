8a60c6baa67163bf75f34866d1eb6869fd321282
unknown <jeremimanogi@gmail.com>
2023-11-15 22:39:00 +0700

    add navigator item model

---
+++ lib/navigator_item_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NavigationItemModel {
+  final String label;
+  final IconData icon;
+
+  NavigationItemModel({required this.label, required this.icon});
+}
